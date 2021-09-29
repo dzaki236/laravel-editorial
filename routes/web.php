4cf38ce691e124acd767baeff39d218bb07e7ea7
dzaki236 <dzakiahnafz@gmail.com>
2021-09-29 22:49:07 +0700

    kepo...

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/pswrd/ku', function () {
+    return "dzakiganteng123~!&[]"."jangan di copy anjay";
+});
